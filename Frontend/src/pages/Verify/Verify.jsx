5f27d5a5ad3d898fdb0090f0e1ec6d11680d976c
Sheetaljain10 <sheetal.is21@bmsce.ac.in>
2024-08-28 19:26:56 +0530

    Frontend Verified

--- Frontend/src/pages/Verify/Verify.jsx
+++ Frontend/src/pages/Verify/Verify.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from 'react';
-import './Verify.css'
+import './verify.css';
 import {useNavigate, useSearchParams} from 'react-router-dom'
 import axios from 'axios';
 import {StoreContext} from "../../context/StoreContext"
